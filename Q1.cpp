3bdfebe7d32e4a156b9de6a383a06237d65fe05a
Aayushi-Uniyal <auniyal_be24@thapar.edu>
2025-09-17 23:42:07 +0530

    Update Q1.cpp

--- Q1.cpp
+++ Q1.cpp
@@ -11,13 +11,19 @@ class LinkedList {
 public:
     LinkedList() { head = NULL; }
 
+    ~LinkedList() { while (head) deleteBegin(); }
+
     void insertBegin(int x) {
-        Node* t = new Node{x, head};
+        Node* t = new Node;
+        t->data = x;
+        t->next = head;
         head = t;
     }
 
     void insertEnd(int x) {
-        Node* t = new Node{x, NULL};
+        Node* t = new Node;
+        t->data = x;
+        t->next = NULL;
         if (!head) { head = t; return; }
         Node* p = head;
         while (p->next) p = p->next;
@@ -28,7 +34,9 @@ public:
         Node* p = head;
         while (p && p->data != key) p = p->next;
         if (!p) { cout << "Key not found\n"; return; }
-        Node* t = new Node{x, p->next};
+        Node* t = new Node;
+        t->data = x;
+        t->next = p->next;
         p->next = t;
     }
 
@@ -73,3 +81,31 @@ public:
         cout<<endl;
     }
 };
+
+int main() {
+    LinkedList list;
+    list.insertBegin(10);
+    list.insertEnd(20);
+    list.insertEnd(30);
+    list.insertAfter(20, 25);
+
+    cout << "List after insertions: ";
+    list.display();
+
+    list.deleteBegin();
+    cout << "After deleting from beginning: ";
+    list.display();
+
+    list.deleteEnd();
+    cout << "After deleting from end: ";
+    list.display();
+
+    list.deleteKey(25);
+    cout << "After deleting key 25: ";
+    list.display();
+
+    list.search(20);
+    list.search(100);
+
+    return 0;
+}
